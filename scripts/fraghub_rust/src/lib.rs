3fff4775dd3b9d6debba6c09900171927f5989f0
adablanc <axel.dablanc@univ-tlse3.fr>
2026-06-14 10:03:34 +0200

    Integrate SPLASH generation in Rust backend: replace Python SPLASH logic with optimized Rust implementation, update dependencies (`hex`, `sha2`), and extend PyO3 bindings.

--- scripts/fraghub_rust/src/lib.rs
+++ scripts/fraghub_rust/src/lib.rs
@@ -1,9 +1,10 @@
 use pyo3::prelude::*;
 
-// On déclare tes deux modules
+// Déclaration de vos modules
 pub mod globals_vars;
 pub mod loading_db;
 pub mod convertors;
+pub mod splash_generator; // Ajout du module splash
 
 #[pymodule]
 fn fraghub_rust(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -15,7 +16,6 @@ fn fraghub_rust(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(loading_db::load_instrument_tree, m)?)?;
 
     // --- Partie 2 : Convertisseurs (Étape 1 de parsing_to_dict) ---
-    m.add_function(wrap_pyfunction!(convertors::keys_convertor::convert_keys, m)?)?;
     m.add_function(wrap_pyfunction!(convertors::loaders::generate_file_hash, m)?)?;
     m.add_function(wrap_pyfunction!(convertors::loaders::load_spectrum_list_from_msp, m)?)?;
     m.add_function(wrap_pyfunction!(convertors::loaders::load_spectrum_list_from_mgf, m)?)?;
@@ -25,6 +25,9 @@ fn fraghub_rust(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(convertors::json_to_dict::json_to_dict_processing, m)?)?;
     m.add_function(wrap_pyfunction!(convertors::mgf_to_dict::mgf_to_dict_processing, m)?)?;
     m.add_function(wrap_pyfunction!(convertors::msp_to_dict::msp_to_dict_processing, m)?)?;
+
+    // --- Partie 3 : Générateur de SPLASH ---
+    m.add_function(wrap_pyfunction!(splash_generator::generate_splash_processing, m)?)?;
 
     Ok(())
 }
